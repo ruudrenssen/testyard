ff9d00406d2d2c0e2accc1a11144d62f585ee44e
Ruud Renssen <ruud.renssen@gmail.com>
2022-06-08 21:46:21 +0200

    basic setup templating and partials

--- app/views/home/home.router.js
+++ app/views/home/home.router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-	res.render('dashboard');
+	res.render('home');
 });
 
 module.exports = router;
