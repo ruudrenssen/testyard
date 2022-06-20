c8af5012e85f8611bd2d983bd20deb5e58c06171
Ruud <ruud.renssen@gmail.com>
2022-06-20 13:37:56 +0200

    startr table factory

--- app/views/experiments/table.service.js
+++ app/views/experiments/table.service.js
@@ -1,20 +1,36 @@
-const table = {
-    caption: 'Number notation by country',
-    values: [
-        {
-            title: 'Large monetary value with decimals',
-            type: 'number',
-            value: 1264194174.758942
-        },
-        {
-            title: 'Date',
-            type: 'Date',
-            value: Date.now()
-        }
-    ],
-    cultures: [
-        'nl-NL','nl-BE','de-DE','fr-FR','zh-Hans-CN'
-    ]
-};
+const languageCultureList = require('../../data/tables/language-culture-names-list.json');
+const numberModel = require('../../data/tables/numbers.json');
 
-module.exports = { table };
+const tables = [];
+
+const buildTable = function (model) {
+    const options = model.options;
+    const rows = languageCultureList.map(culture => culture.cultureInfoCode);
+    const table = {
+        caption: model?.caption,
+        value: model?.value,
+        format: model?.intl,
+        header: [],
+        body: [],
+        footer: []
+    }
+
+    table.body = rows.map(row => {
+        const rowValues = [];
+        rowValues.push(row);
+        
+        options.forEach(option => {
+            rowValues.push(option.values.map(value => value));
+        });
+
+        return rowValues;
+    });
+
+    return table;
+}
+
+tables.push(buildTable(numberModel));
+
+console.log(tables[0]);
+
+module.exports = { tables };
