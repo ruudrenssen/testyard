7323bc0797d964258f06b3b5c4ee91d55aa0f13c
Ruud Renssen <ruud.renssen@gmail.com>
2022-09-21 14:33:11 +0200

    all kind of stuff - update eslint - update port for app - set up d3 - surpress circular warnings for d3 in eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,30 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { babel } from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
+import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 // import { terser } from 'rollup-plugin-terser';
 
 export default {
-	input: 'source/scripts/index.mjs',
-	output: {
-		file: 'static/js/main.mjs',
-		format: 'esm',
-	},
-	plugins: [
-		commonjs(),
-		nodeResolve(),
-		babel({
-			babelHelpers: 'bundled',
-		}),
-		// terser(),
-	],
+  input: "source/scripts/index.mjs",
+  output: {
+    file: "static/js/main.mjs",
+    format: "esm",
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    babel({
+      babelHelpers: "bundled",
+    }),
+    // terser(),
+  ],
+  onwarn(warning, warn) {
+    // Surpress D3 warning about circular dependencies: https://github.com/rollup/rollup/pull/1491#issuecomment-321714821
+    if (warning.importer === "node_modules/d3-transition/src/transition/index.js") return;
+    if (warning.importer === "node_modules/d3-selection/src/selection/index.js") return;
+    if (warning.importer === "node_modules/d3-interpolate/src/value.js") return;
+
+    // Use default for everything else
+    warn(warning);
+  },
 };
+
--- source/scripts/modules/d3.mjs
+++ source/scripts/modules/d3.mjs
@@ -1,10 +1,46 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 class D3 {
   constructor(element) {
+    this.data = {};
     this.element = element;
+    this.init();
+  }
 
-    console.log(d3);
+  async init() {
+    this.data = await D3.loadData("circuits");
+
+    D3.tabulate(this.element, this.data);
+  }
+
+  static tabulate(parent, data) {
+    console.log(data);
+    const headers = ["a", "b", "c", "d"];
+
+    let table = d3.select(parent)
+      , thead = table.append("thead")
+      , tbody = table.append("tbody");
+
+    thead.append("tr")
+      .selectAll("th")
+      .data(headers)
+      .enter()
+      .append("th")
+      .text(function (headers) { return headers; });
+
+    return table;
+  }
+
+  static async loadData(url, limit = 30, offset = 0 ) {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    return fetch(`http://ergast.com/api/f1/${url}.json?limit=${limit}&offset=${offset}`, requestOptions)
+      .then((response) => response.text())
+      .then((result) => result)
+      .catch((error) => console.error("error", error));
   }
 }
 
